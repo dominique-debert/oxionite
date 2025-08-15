99ba502e84a5dceba1aa1465b561b008b2de92ee
Jzahnny <alemem64@gmail.com>
2025-08-15 21:11:19 +0900

    removed unusing config

--- site.config.ts
+++ site.config.ts
@@ -8,21 +8,13 @@ export default siteConfig({
   // The database id for the blog (required)
   rootNotionDatabaseId: '21df2d475c31812dae49d1b1735e02b4',
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
-
   // basic site info (required)
   name: 'Noxionite',
   domain: 'noxionite.vercel.app',
   author: 'Jaewan Shin',
 
   // open graph metadata (optional)
-  description: 'Effortlessly turn your Notion pages into a stunning blog.',
-
-  // locale configuration (optional)
-  locale,
-
+  description: 'Your Notion pages, reborn as a stunning blog',
 
   // hero section (optional)
   heroAssets: [
@@ -135,49 +127,10 @@ export default siteConfig({
     // newsletter: '#' // optional personal newsletter URL
   },
 
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
-  defaultPageCoverPosition: 0.5,
+  // locale configuration
+  locale,
 
-  // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
-
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
-  isRedisEnabled: false,
-
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
-  //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
-
-  // whether to use the default notion navigation style or a custom one with links to
-  // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default',
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ],
-
-  //   }
-  // ],
-
-  // Incremental Static Regeneration (ISR) configuration (optional)
+  // Incremental Static Regeneration (ISR) configuration
   isr: {
     revalidate: 60 // revalidate time in seconds
   }
